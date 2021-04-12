0afc9fd820d23b9f10527d63dfa04cec9ec120d5
hachiojidev <hachiojidev@protonmail.com>
2021-04-12 12:01:27 +0900

    feat(ifo): Details FAQ

--- src/views/Ifos/components/IfoQuestions.tsx
+++ src/views/Ifos/components/IfoQuestions.tsx
@@ -1,28 +1,27 @@
 import React from 'react'
 import styled from 'styled-components'
-import { Text, Heading, BaseLayout, Button, LinkExternal, Flex, Image } from '@pancakeswap-libs/uikit'
+import { Text, Heading, Card, CardHeader, CardBody, Flex } from '@pancakeswap-libs/uikit'
 import useI18n from 'hooks/useI18n'
+import FoldableText from 'components/FoldableText'
 
-const LaunchIfoCallout = styled(BaseLayout)`
-  border-top: 2px solid ${({ theme }) => theme.colors.textSubtle};
-  display: grid;
-  grid-template-columns: 1fr;
-  grid-gap: 32px;
-  margin: 0 auto;
-  padding: 32px 0;
+const ImageWrapper = styled.div`
+  flex: none;
+  order: 2;
+  width: 224px;
 
-  ${({ theme }) => theme.mediaQueries.sm} {
-    grid-template-columns: 1fr 1fr;
+  ${({ theme }) => theme.mediaQueries.md} {
+    order: 1;
   }
 `
 
-const List = styled.ul`
-  color: ${({ theme }) => theme.colors.text};
-  margin-bottom: 16px;
+const DetailsWrapper = styled.div`
+  order: 1;
+  margin-bottom: 40px;
 
-  & > li {
-    line-height: 1.4;
-    margin-bottom: 8px;
+  ${({ theme }) => theme.mediaQueries.md} {
+    order: 2;
+    margin-bottom: 0;
+    margin-left: 40px;
   }
 `
 
@@ -30,65 +29,57 @@ const IfoQuestions = () => {
   const TranslateString = useI18n()
 
   return (
-    <LaunchIfoCallout>
-      <div>
-        <Heading as="h2" size="lg" color="secondary" mb="16px">
-          {TranslateString(592, 'How to take part')}
-        </Heading>
-        <Heading mb="16px">{TranslateString(594, 'Before Sale')}:</Heading>
-        <List>
-          <li>{TranslateString(596, 'Buy CAKE and BNB tokens')}</li>
-          <li>{TranslateString(598, 'Get CAKE-BNB LP tokens by adding CAKE and BNB liquidity')}</li>
-        </List>
-        <Flex mb="16px">
-          <LinkExternal href="https://exchange.pancakeswap.finance/#/swap" mr="16px">
-            {TranslateString(1060, 'Buy CAKE')}
-          </LinkExternal>
-          <LinkExternal href="https://exchange.pancakeswap.finance/#/add/BNB/0x0E09FaBB73Bd3Ade0a17ECC321fD13a19e81cE82">
-            {TranslateString(1062, 'Get LP tokens')}
-          </LinkExternal>
-        </Flex>
-        <Heading mb="16px">{TranslateString(600, 'During Sale')}:</Heading>
-        <List>
-          <li>{TranslateString(602, 'While the sale is live, commit your CAKE-LP tokens to buy the IFO tokens')}</li>
-        </List>
-        <Heading mb="16px">{TranslateString(604, 'After Sale')}:</Heading>
-        <List>
-          <li>{TranslateString(606, 'Claim the tokens you bought, along with any unspent funds.')}</li>
-          <li>{TranslateString(608, 'Done!')}</li>
-        </List>
-        <Text as="div" pt="16px">
-          <Button
-            as="a"
-            variant="secondary"
-            href="https://docs.pancakeswap.finance/core-products/ifo-initial-farm-offering"
-          >
-            {TranslateString(610, 'Read more')}
-          </Button>
-        </Text>
-      </div>
-      <div>
-        <Image src="/images/ifo-bunny.svg" alt="ifo bunny" width={436} height={406} responsive />
-        <div>
-          <Heading as="h2" size="lg" color="secondary" mb="16px">
-            {TranslateString(512, 'Want to launch your own IFO?')}
-          </Heading>
-          <Text mb={3}>
-            {TranslateString(
-              514,
-              'Launch your project with PancakeSwap, Binance Smart Chain’s most-used AMM project and liquidity provider, to bring your token directly to the most active and rapidly growing community on BSC.',
-            )}
-          </Text>
-          <Button
-            as="a"
-            href="https://docs.google.com/forms/d/e/1FAIpQLScGdT5rrVMr4WOWr08pvcroSeuIOtEJf1sVdQGVdcAOqryigQ/viewform"
-            external
-          >
-            {TranslateString(516, 'Apply to launch')}
-          </Button>
-        </div>
-      </div>
-    </LaunchIfoCallout>
+    <Flex alignItems={['center', null, null, 'start']} flexDirection={['column', null, null, 'row']}>
+      <ImageWrapper>
+        <img src="/images/ifo-bunny.png" alt="ifo bunny" width="224px" height="208px" />
+      </ImageWrapper>
+      <DetailsWrapper>
+        <Card>
+          <CardHeader>
+            <Heading size="lg" color="secondary">
+              {TranslateString(999, 'Details')}
+            </Heading>
+          </CardHeader>
+          <CardBody>
+            <FoldableText
+              mb="24px"
+              title={TranslateString(999, "What's the difference between a Basic Sale and Unlimited Sale?")}
+            >
+              <Text color="textSubtle" as="p">
+                TBD
+              </Text>
+            </FoldableText>
+            <FoldableText mb="24px" title={TranslateString(999, 'Which sale should I commit to? Can I do both?')}>
+              <Text color="textSubtle" as="p">
+                {TranslateString(999, 'You can choose one or both at the same time!')}
+              </Text>
+              <Text color="textSubtle" as="p">
+                {TranslateString(
+                  999,
+                  'We recommend using the Basic Sale first for most users, because the amount of tokens you can get for your LP Tokens should be a little higher than in the Unlimited Sale.',
+                )}
+              </Text>
+            </FoldableText>
+            <FoldableText mb="24px" title={TranslateString(999, 'Will my purchase')}>
+              <Text color="textSubtle" as="p">
+                {TranslateString(
+                  999,
+                  'Response to open question goes here. Here is text answering the question which is open. Response to open question goes here. Here is text answering the question which is open.',
+                )}
+              </Text>
+            </FoldableText>
+            <FoldableText title={TranslateString(999, 'Open question')}>
+              <Text color="textSubtle" as="p">
+                {TranslateString(
+                  999,
+                  'Response to open question goes here. Here is text answering the question which is open. Response to open question goes here. Here is text answering the question which is open.',
+                )}
+              </Text>
+            </FoldableText>
+          </CardBody>
+        </Card>
+      </DetailsWrapper>
+    </Flex>
   )
 }
 
